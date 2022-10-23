cd55d7640773b9948e3414f0c5f6e0fb1fead0e2
Chris Mimm <chris.mimm@musclefood.com>
2022-10-23 22:10:02 +0100

    Add clock project

---
+++ clock/src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+    return (
+        <div className="app-container">
+            <div className="clock">
+                <div className="arms">
+                    <div className="second-arm"></div>
+                    <div className="minute-arm"></div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default App;
